999747dd0651f77a469205fd3e57ad91c0963d04
Comyar Zaheri <comyarzaheri@gmail.com>
2016-12-02 11:48:44 -0800

    Updated .travis.yml

---
+++ Jupiter-macOS/Jupiter-macOS.h
@@ -0,0 +1,19 @@
+//
+//  Jupiter-macOS.h
+//  Jupiter-macOS
+//
+//  Created by Comyar Zaheri on 12/2/16.
+//  Copyright © 2016 Comyar Zaheri. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Jupiter-macOS.
+FOUNDATION_EXPORT double Jupiter_macOSVersionNumber;
+
+//! Project version string for Jupiter-macOS.
+FOUNDATION_EXPORT const unsigned char Jupiter_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jupiter_macOS/PublicHeader.h>
+
+
---
+++ Jupiter-watchOS/Jupiter-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Jupiter-watchOS.h
+//  Jupiter-watchOS
+//
+//  Created by Comyar Zaheri on 12/2/16.
+//  Copyright © 2016 Comyar Zaheri. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Jupiter-watchOS.
+FOUNDATION_EXPORT double Jupiter_watchOSVersionNumber;
+
+//! Project version string for Jupiter-watchOS.
+FOUNDATION_EXPORT const unsigned char Jupiter_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jupiter_watchOS/PublicHeader.h>
+
+
